864c3f1b62d727daf6ef9aa5fb0bd6858ffc67e3
umairahmed88 <umairahmed_88@ymail.com>
2023-03-15 07:56:26 +0500

    created a web 3.0 Domain site

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,17 +4,54 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+	return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 async function main() {
+	// Setup accounts & variables
+	const [deployer] = await ethers.getSigners();
+	const NAME = "ETH Daddy";
+	const SYMBOL = "ETHD";
 
+	// Deploy contract
+	const ETHDaddy = await ethers.getContractFactory("ETHDaddy");
+	const ethDaddy = await ETHDaddy.deploy(NAME, SYMBOL);
+	await ethDaddy.deployed();
+
+	console.log(`Deployed Domain Contract at: ${ethDaddy.address}\n`);
+
+	const names = [
+		"ua.eth",
+		"au.eth",
+		"mom.eth",
+		"dad.eth",
+		"oxygen.eth",
+		"carbon.eth",
+	];
+	const costs = [
+		tokens(10),
+		tokens(25),
+		tokens(15),
+		tokens(2.5),
+		tokens(3),
+		tokens(1),
+	];
+
+	for (var i = 0; i < 6; i++) {
+		const transaction = await ethDaddy
+			.connect(deployer)
+			.list(names[i], costs[i]);
+		await transaction.wait();
+
+		console.log(`Listed Domain ${i + 1}: ${names[i]}`);
+	}
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,9 +1,113 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+	return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("ETHDaddy", () => {
+	let ethDaddy;
+	let deployer, owner1;
 
-})
+	const NAME = "ETH Daddy";
+	const SYMBOL = "ETHD";
+
+	beforeEach(async () => {
+		// Setup accounts
+		[deployer, owner1] = await ethers.getSigners();
+
+		// Deploy contracts
+		const ETHDaddy = await ethers.getContractFactory("ETHDaddy");
+		ethDaddy = await ETHDaddy.deploy("ETH Daddy", "ETHD");
+
+		// List a domain
+		const transaction = await ethDaddy
+			.connect(deployer)
+			.list("ua.eth", tokens(10));
+		await transaction.wait();
+	});
+
+	describe("Deployments", () => {
+		it("It has name", async () => {
+			const result = await ethDaddy.name();
+			expect(result).to.eq(NAME);
+		});
+		it("It has a symbol", async () => {
+			const result = await ethDaddy.symbol();
+			expect(result).to.eq(SYMBOL);
+		});
+		it("Sets the owner", async () => {
+			const result = await ethDaddy.owner();
+			expect(result).to.eq(deployer.address);
+		});
+		it("Returns the max supply", async () => {
+			const result = await ethDaddy.maxSupply();
+			expect(result).to.eq(1);
+		});
+		it("Returns the total supply", async () => {
+			const result = await ethDaddy.totalSupply();
+			expect(result).to.eq(0);
+		});
+	});
+
+	describe("Domain", () => {
+		it("Returns domain attributes", async () => {
+			let domain = await ethDaddy.getDomain(1);
+			expect(domain.name).to.be.eq("ua.eth");
+			expect(domain.cost).to.be.equal(tokens(10));
+			expect(domain.isOwned).to.be.equal(false);
+		});
+	});
+
+	describe("Minting", () => {
+		const ID = 1;
+		const AMOUNT = ethers.utils.parseUnits("10", "ether");
+
+		beforeEach(async () => {
+			const transaction = await ethDaddy
+				.connect(owner1)
+				.mint(ID, { value: AMOUNT });
+			await transaction.wait();
+		});
+
+		it("Updates the owner", async () => {
+			const owner = await ethDaddy.ownerOf(ID);
+			expect(owner).to.be.eq(owner1.address);
+		});
+		it("Updates the domain status", async () => {
+			const domain = await ethDaddy.getDomain(ID);
+			expect(domain.isOwned).to.be.eq(true);
+		});
+		it("Updates the smart contract balance ", async () => {
+			const result = await ethDaddy.getBalance();
+			expect(result).to.be.eq(AMOUNT);
+		});
+	});
+
+	describe("Withdrawing", () => {
+		const ID = 1;
+		const AMOUNT = ethers.utils.parseUnits("10", "ether");
+		let balanceBefore;
+
+		beforeEach(async () => {
+			const balanceBefore = await ethers.provider.getBalance(deployer.address);
+
+			let transaction = await ethDaddy
+				.connect(owner1)
+				.mint(ID, { value: AMOUNT });
+			await transaction.wait();
+
+			transaction = await ethDaddy.connect(deployer).withdraw();
+			await transaction.wait();
+		});
+
+		it("Updates the owner balance", async () => {
+			const owner = await ethDaddy.ownerOf(ID);
+			expect(owner).to.be.eq(owner1.address);
+		});
+		it("Updates the contract balance ", async () => {
+			const result = await ethDaddy.getBalance();
+			expect(result).to.be.eq(0);
+		});
+	});
+});
